4a790088a821cc2fadafa115645b44ed2c02b74b
Nadezhda Komarova <naduha.9612@gmail.com>
2023-10-02 02:51:38 +0300

    feat: add search input

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const imageContainer = document.querySelector('.image-container');
-
+const searchIcon = document.querySelector('.search_search-icon')
+const searchForm = document.querySelector('.search_form');
+const searchText = document.querySelector('.search_text');
 
 const url = 'https://api.unsplash.com/search/photos?query=urban&per_page=30&orientation=landscape&client_id=mN3eeA-5RHLHn6zS9_tetXDagPa1NweYkcwJicCW8Xo';
 let data = '';
+let searchWord = '';
 
 async function getData() {
     const res = await fetch(url);
@@ -19,4 +22,18 @@ async function getData() {
           }
           )
   }
+
+ searchIcon.addEventListener('click', function() {
+    searchWord = searchText.value;
+  });
+
+  document.addEventListener('keydown', function(e) {
+    if(e.code === 'Enter') {
+        e.preventDefault();
+        searchWord = searchText.value;
+        console.log(searchWord);
+  };
+})
+
+
   getData();
