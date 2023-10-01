03041a6ca3b17c872418c05200b3c85a637eceb9
Nadezhda Komarova <naduha.9612@gmail.com>
2023-10-02 02:59:53 +0300

    feat: add search clean

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const imageContainer = document.querySelector('.image-container');
 const searchIcon = document.querySelector('.search_search-icon')
-const searchForm = document.querySelector('.search_form');
 const searchText = document.querySelector('.search_text');
+const searchClean = document.querySelector('.search_clean');
 
 const url = 'https://api.unsplash.com/search/photos?query=urban&per_page=30&orientation=landscape&client_id=mN3eeA-5RHLHn6zS9_tetXDagPa1NweYkcwJicCW8Xo';
 let data = '';
 let searchWord = '';
 
-async function getData() {
-    const res = await fetch(url);
-    data = await res.json();
-    console.log(data.results);
-    let result = data.results;
-    console.log(result);
-    result.map((el) => {
-        const img = document.createElement('img');
-        img.classList.add('gallery-img')
-        img.src = el.urls.regular;
-        img.alt = `image`;
-        imageContainer.append(img);
-          }
-          )
-  }
+// async function getData() {
+//     const res = await fetch(url);
+//     data = await res.json();
+//     let result = data.results;
+//     result.map((el) => {
+//         const img = document.createElement('img');
+//         img.classList.add('gallery-img')
+//         img.src = el.urls.regular;
+//         img.alt = `image`;
+//         imageContainer.append(img);
+//           }
+//           )
+//   }
 
  searchIcon.addEventListener('click', function() {
     searchWord = searchText.value;
@@ -33,7 +31,11 @@ async function getData() {
         searchWord = searchText.value;
         console.log(searchWord);
   };
+});
+
+searchClean.addEventListener('click', function() {
+    searchText.value = '';
 })
 
 
-  getData();
+//   getData();
