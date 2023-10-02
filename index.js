085bfadddec2e5f158c860b1075dd3fc5588d292
Nadezhda Komarova <naduha.9612@gmail.com>
2023-10-02 03:33:24 +0300

    feat: add search

--- index.js
+++ index.js
@@ -6,23 +6,43 @@ const searchClean = document.querySelector('.search_clean');
 const url = 'https://api.unsplash.com/search/photos?query=urban&per_page=30&orientation=landscape&client_id=mN3eeA-5RHLHn6zS9_tetXDagPa1NweYkcwJicCW8Xo';
 let data = '';
 let searchWord = '';
+let dynamicURL = `https://api.unsplash.com/search/photos?query=${searchWord}&per_page=30&orientation=landscape&client_id=mN3eeA-5RHLHn6zS9_tetXDagPa1NweYkcwJicCW8Xo`;
 
-// async function getData() {
-//     const res = await fetch(url);
-//     data = await res.json();
-//     let result = data.results;
-//     result.map((el) => {
-//         const img = document.createElement('img');
-//         img.classList.add('gallery-img')
-//         img.src = el.urls.regular;
-//         img.alt = `image`;
-//         imageContainer.append(img);
-//           }
-//           )
-//   }
+
+async function getData() {
+    const res = await fetch(url);
+    data = await res.json();
+    let result = data.results;
+    result.map((el) => {
+        const img = document.createElement('img');
+        img.classList.add('gallery-img')
+        img.src = el.urls.regular;
+        img.alt = `image`;
+        imageContainer.append(img);
+          }
+          )
+  }
+
+  async function searchData() {
+    const res = await fetch(dynamicURL);
+    data = await res.json();
+    let result = data.results;
+    result.map((el) => {
+        const img = document.createElement('img');
+        img.classList.add('gallery-img')
+        img.src = el.urls.regular;
+        img.alt = `image`;
+        imageContainer.append(img);
+          }
+          )
+  }
 
  searchIcon.addEventListener('click', function() {
     searchWord = searchText.value;
+    console.log(searchWord);
+    dynamicURL = `https://api.unsplash.com/search/photos?query=${searchWord}&per_page=30&orientation=landscape&client_id=mN3eeA-5RHLHn6zS9_tetXDagPa1NweYkcwJicCW8Xo`;
+    console.log(dynamicURL);
+    searchData();
   });
 
   document.addEventListener('keydown', function(e) {
@@ -30,6 +50,9 @@ let searchWord = '';
         e.preventDefault();
         searchWord = searchText.value;
         console.log(searchWord);
+        dynamicURL = `https://api.unsplash.com/search/photos?query=${searchWord}&per_page=30&orientation=landscape&client_id=mN3eeA-5RHLHn6zS9_tetXDagPa1NweYkcwJicCW8Xo`;
+        console.log(dynamicURL);
+        searchData();
   };
 });
 
@@ -38,4 +61,4 @@ searchClean.addEventListener('click', function() {
 })
 
 
-//   getData();
+  getData();
